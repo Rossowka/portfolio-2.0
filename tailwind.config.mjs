a8642045b2c7d7f5d979f812e2924cc7c09b1ff8
s.rossow@hawe.de <s.rossow@hawe.de>
2025-02-06 21:09:37 +0100

    added dependencies and updated tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -34,20 +34,22 @@ export default {
     plugin(function ({ addBase, addComponents }) {
       addBase({
         h1: {
-          "@apply font-bold text-[4rem] leading-[1] md:text-[6rem]": {},
+          "@apply font-bold text-[4rem] leading-normal md:text-[5rem] lg:text-[6rem]":
+            {},
         },
         h2: {
-          "@apply font-medium text-[3rem] leading-[1.25] md:text-[5rem]": {},
+          "@apply font-medium text-[3rem] leading-tight md:text-[4rem] lg:text-[5rem]":
+            {},
         },
         h3: {
           "@apply font-medium text-lg": {},
         },
-        h4: {
-          "@apply font-medium text-[1.75rem] leading-[1.15]": {},
-        },
-        h5: {
-          "@apply font-medium text-[0.75rem] md:text-[1rem] leading-[1.05]": {},
-        },
+        // h4: {
+        //   "@apply font-medium text-[1.75rem] leading-[1.15]": {},
+        // },
+        // h5: {
+        //   "@apply font-medium text-[0.75rem] md:text-[1rem] leading-[1.05]": {},
+        // },
         p: {
           "@apply font-medium text-[1.125rem] leading-relaxed": {},
         },
@@ -55,6 +57,16 @@ export default {
       addComponents({
         ".container": {
           "@apply max-w-[77.5rem] mx-auto px-4 lg:px-8": {},
+        },
+        ".page-title": {
+          "@apply font-medium text-[3rem] md:text-[4rem] lg:text-[5rem] tracking-tight leading-tight":
+            {},
+        },
+        ".title": {
+          "@apply text-2xl lg:text-[2.5rem] leading-tight mb-8 lg:mb-16": {},
+        },
+        ".eyebrow": {
+          "@apply text-f-primary/70 whitespace-nowrap leading-relaxed": {},
         },
       });
     }),
