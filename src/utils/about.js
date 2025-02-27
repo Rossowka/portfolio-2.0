99328823483d08ff9db9c05f5b9ea5963290d2ce
s.rossow@hawe.de <s.rossow@hawe.de>
2025-02-27 23:04:34 +0100

    added about data file

---
+++ src/utils/about.js
@@ -0,0 +1,99 @@
+export const story = [
+  {
+    heading: "curiosity-driven design",
+    text: "I believe in stories. Every team and company has one worth telling. To engage with them, I start by carefuly listening to both users and businesses. By asking thoughtful questions I uncover that sweet spot where their needs meet and meaningful connections can happen. That’s where I thrive as a designer, crafting digital experiences that don't just solve problems, but create lasting impressions.",
+  },
+  {
+    heading: "hands-on design across mediums",
+    text: "While I specialize in designing and developing digital products, I’m a trained architect who enjoys working on physical projects. I tap into my creativity through DIY projects, painting and architectural exploration.",
+  },
+  {
+    heading: "experiences speak louder than words",
+    text: "In both professional and personal life, I am looking for ways to create memorable experiences that spark curiosity, build trust, and connect. Because the most powerful stories aren't just told. They're experienced.",
+  },
+];
+
+export const careerInNumbers = [
+  {
+    number: "4+",
+    text: "Years of experience in creative professions",
+    subtext:
+      "2 years in Architecture designing buildings and coordinating construction & 2+ years in UX design designing B2B and B2C digital products.",
+  },
+  {
+    number: "8/22+",
+    text: "Products / Features Designed",
+    subtext: "3 out of 8 were new products",
+  },
+  {
+    number: ">60",
+    text: "People trained through onboarding sessions and buddy programme",
+    subtext:
+      "I also mentored team members as they transitioned into specialized roles, sharing what I know and helping them build the skills they needed to succeed.",
+  },
+];
+
+export const services = [
+  {
+    title: "Research & Strategy",
+    method: [
+      "Quantitative Research",
+      "UX Audit",
+      "Customer Journey Mapping",
+      "Usability Testing",
+      "Website Analytics",
+      "Stakeholder Interviews",
+    ],
+  },
+  {
+    title: "Product Design",
+    method: [
+      "Webdesign",
+      "Web & Mobile Design",
+      "Design Systems",
+      "Product Concept",
+      "Prototyping",
+      "Product Validation",
+    ],
+  },
+  {
+    title: "Development",
+    method: [
+      "Frontend Development",
+      "Web Development",
+      "Low-Code Development",
+      "GSAP Animations",
+      "SEO & Hosting",
+      "CMS Integrations",
+    ],
+  },
+];
+
+export const photos = [];
+
+export const superpowers = [
+  {
+    heading: "empathetic leadership",
+    text: "With a natural ability to connect with people, I am a compassionate collaborator who fosters supportive environment.",
+  },
+  {
+    heading: "problem-solving mindset",
+    text: "With my eagle eye I spot challenges before they escalate. I also come up with creative solutions to keep obstacles at bay.",
+  },
+  {
+    heading: "flexibility & adaptability",
+    text: "Comfortable in dynamic environments, my adaptability shines through in  situations involving uncertainty or lack of information. ",
+  },
+  {
+    heading: "strategic optimization",
+    text: "Finding ways to improve even most complex systems is something I genuinely enjoy. I obsess about efficiency and smart iterations.",
+  },
+  {
+    heading: "self-motivated growth",
+    text: "Learning builds my confidence and I’m always seeking out opportunities to develop my skills and passing on what I’ve learned.",
+  },
+  {
+    heading: "accountability & integrity",
+    text: "The key to my approach is taking full ownership of commitments. I insist on the highest standards and, in my eyes, long term value outweighs quick wins.",
+  },
+];
