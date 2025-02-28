d47f0185a7f0e24bf00af06f7fc7f6b64318ff4c
s.rossow@hawe.de <s.rossow@hawe.de>
2025-02-28 11:30:38 +0100

    added structure to about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,20 +1,21 @@
 // "use client";
 import { Button } from "@/components";
-import { Welcome } from "@/components/About";
+import {
+  AboutSection,
+  CareerInNumbers,
+  Superpowers,
+  Welcome,
+} from "@/components/About";
 
 const About = () => {
   return (
     <>
       <Welcome />
-      <section className="pt-32 pb-8 md:pb-24 md:pt-48 lg:pb-24 relative">
-        <div className="container flex justify-center">
-          <Button
-            label="see my work"
-            className="text-f-inverse"
-            href="/projects"
-          />
-        </div>
+      <section className="container pt-16 lg:pt-32 pb-8 md:pb-24 md:pt-48 lg:pb-24 relative flex flex-col lg:flex-row gap-16 lg:justify-between">
+        <CareerInNumbers />
+        <AboutSection />
       </section>
+      <Superpowers />
     </>
   );
 };
