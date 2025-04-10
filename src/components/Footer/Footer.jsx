686e057d04726bc4a7bca830ffefd359e061fb38
s.rossow@hawe.de <s.rossow@hawe.de>
2025-04-10 14:03:38 +0200

    fix Footer consistency and removed mail link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,15 +11,15 @@ const Footer = () => {
   const artBanner = "/images/banner.png";
   return (
     <footer
-      className="pt-32 lg:pt-48 overflow-hidden"
+      className="pt-32 lg:pt-48"
       id="contact"
     >
       {/* contact cta */}
       <div className="container flex flex-col mb-16 lg:mb-32">
         <div className="relative">
           <HeadingXL
-            headingText="let's work together."
-            subheadingText="sparked your curiosity?"
+            headingText="Let's work together."
+            subheadingText="Sparked your curiosity?"
           />
           <ArrowIcon
             className="rotate-[135deg] absolute right-[8%] bottom-0 z-10"
@@ -30,9 +30,9 @@ const Footer = () => {
         {/* banner */}
         <div className="flex flex-col md:flex-row md:gap-8 items-center relative">
           {/* image */}
-          <Link
+          <div
             className="flex-1"
-            href="mailto:sararossow@mail.com?subject=Getting%20in%20touch&body=Hello%20Sara,"
+            // href="mailto:sararossow@mail.com?subject=Getting%20in%20touch&body=Hello%20Sara,"
           >
             <Image
               src={artBanner}
@@ -42,7 +42,7 @@ const Footer = () => {
               className="rounded-2xl w-full h-full object-cover"
               alt="Image of a horizon with mountains and colorful clouds"
             />
-          </Link>
+          </div>
 
           {/* text */}
           <div className="flex-shrink-0">
