a82a6155a0644fcc4ed662e9ba6c988c1b0ef431
s.rossow@hawe.de <s.rossow@hawe.de>
2025-05-26 17:40:45 +0200

    cleaned tailwind config file to reduce bundle size

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
@@ -29,50 +28,6 @@ export default {
       animation: {
         "spin-slow": "spin 20s linear infinite",
       },
-      backgroundImage: {
-        gradient:
-          "linear-gradient(0deg,rgba(0,0,0,.6),rgba(0,0,0,.6)),radial-gradient(at 24% 52%, hsla(210,27%,24%,0.1) 0px, transparent 50%),radial-gradient(at 15% 23%, hsla(24,100%,40%,0.1) 0px, transparent 50%),radial-gradient(at 77% 71%, hsla(24,100%,40%,0.1) 0px, transparent 50%),radial-gradient(at 72% 39%, hsla(210,27%,24%,0.1) 0px, transparent 50%),radial-gradient(at 43% 40%, hsla(24,100%,40%,0.1) 0px, transparent 50%)",
-      },
     },
   },
-  plugins: [
-    plugin(function ({ addBase, addComponents }) {
-      addBase({
-        h1: {
-          "@apply font-bold text-[4rem] leading-normal md:text-[5rem] lg:text-[6rem]":
-            {},
-        },
-        h2: {
-          "@apply font-medium text-[3rem] md:text-[4rem] lg:text-[5rem] tracking-tight leading-tight":
-            {},
-        },
-        h3: {
-          "@apply font-medium text-lg": {},
-        },
-        p: {
-          "@apply font-medium text-lg leading-relaxed": {},
-        },
-      });
-      addComponents({
-        ".container": {
-          "@apply max-w-[77.5rem] mx-auto px-4 lg:px-8": {},
-        },
-        ".page-title": {
-          "@apply font-medium text-[3rem] md:text-[4rem] lg:text-[5rem] tracking-tight leading-tight":
-            {},
-        },
-        ".project-title": {
-          "@apply font-medium text-[2rem] lg:text-[3rem] tracking-tight leading-tight mb-8 lg:mb-8":
-            {},
-        },
-        ".title": {
-          "@apply text-2xl lg:text-[2.5rem] leading-tight mb-8 lg:mb-16": {},
-        },
-        ".eyebrow": {
-          "@apply font-semibold uppercase text-f-primary/40 whitespace-nowrap leading-relaxed text-sm mt-1":
-            {},
-        },
-      });
-    }),
-  ],
 };
