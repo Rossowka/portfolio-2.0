61b7728a3a570b4a164d46a6e795741f46e230fc
s.rossow@hawe.de <s.rossow@hawe.de>
2025-05-26 17:40:11 +0200

    added bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const baseConfig = {
+  // any future settings you want (e.g. images, i18n, etc.)
+};
+
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(baseConfig);
 
 export default nextConfig;
